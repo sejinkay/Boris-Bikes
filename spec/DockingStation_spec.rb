4f7ac6558aaa1160a60f4ed982b80c5c40bc8125
Stuart <stuart.pethurst@gmail.com>
2019-04-02 17:16:10 +0100

    Check if bike is working

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,4 +2,9 @@ require 'DockingStation'
 
 describe DockingStation do
 it {is_expected.to respond_to :release_bike}
+
+it "releases a working bike" do
+  bike = subject.release_bike
+  expect(bike).to be_working
+end
 end
