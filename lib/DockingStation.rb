9515e54463d62e866cc135e9d0a4f91197aba925
Tara Sankhalpara <tsankhalpara@mac.com>
2019-04-03 15:30:41 +0100

    create a test that will raise an error if the docking station is full

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -7,6 +7,7 @@ class DockingStation
   end
 
   def dock(bike)
+    fail 'Docking station full' if @bike
     @bike = bike
   end
 
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -16,6 +16,13 @@ describe '#release_bike' do
   end
 end
 
+describe '#dock' do
+  it "raises an error if the docking station is full" do
+    subject.dock(Bike.new)
+    expect{subject.dock(Bike.new)}.to raise_error 'Docking station full'
+  end
+end
+
 #it "releases a working bike" do
 #  bike = subject.release_bike
 #  expect(bike).to be_working
