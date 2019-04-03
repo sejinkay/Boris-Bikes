589b814ad0c9e3d13db9e40d4223deb19a9790f1
Tara Sankhalpara <tsankhalpara@mac.com>
2019-04-03 16:28:55 +0100

    add private methods and refactor code to use empty? and full?

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -5,13 +5,23 @@ class DockingStation
     @bike = []
   end
   def release_bike
-    fail 'No bikes available' if @bike.empty?
+    fail 'No bikes available' if empty?
     @bike.pop
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bike.count >= 20
+    fail 'Docking station full' if full?
     @bike << bike
+  end
+
+  private
+
+  def full?
+    @bike.count >= 20
+  end
+
+  def empty?
+    @bike.empty?
   end
 
   attr_reader :bike
