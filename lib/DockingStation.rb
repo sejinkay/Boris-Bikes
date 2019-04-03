74a42b61eeeec527faa97be892f6bcda0d16ef45
sejinkay <sejinkay@gmail.com>
2019-04-03 14:38:06 +0100

    added dock method and feature test

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -4,4 +4,11 @@ class DockingStation
   def release_bike
     Bike.new
   end
+  def dock(bike)
+    bike
+  end
+
+  def bike
+  end
+
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -7,4 +7,14 @@ it "releases a working bike" do
   bike = subject.release_bike
   expect(bike).to be_working
 end
+
+it {is_expected.to respond_to(:dock).with(1).argument}
+
+it {is_expected.to respond_to(:bike)}
+
+it 'docks a bike' do
+  bike = Bike.new
+  expect(subject.dock(bike)).to eq bike
+end
+
 end
