fd1d641cc2995468437874dca074624e2b34792b
sejinkay <sejinkay@gmail.com>
2019-04-03 16:22:55 +0100

    changed the capacity of the docking station to 20

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,16 +1,18 @@
 require_relative 'bike'
 
 class DockingStation
+  def initialize
+    @bike = []
+  end
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @bike.empty?
+    @bike.pop
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bike
-    @bike = bike
+    fail 'Docking station full' if @bike.count >= 20
+    @bike << bike
   end
 
   attr_reader :bike
-
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -18,9 +18,14 @@ end
 
 describe '#dock' do
   it "raises an error if the docking station is full" do
-    subject.dock(Bike.new)
+    20.times {subject.dock(Bike.new)}
     expect{subject.dock(Bike.new)}.to raise_error 'Docking station full'
   end
+
+  #it 'docks a bike' do
+  #  subject.dock(bike)
+  #  expect(subject.dock(bike)).to eq @bike
+  #end
 end
 
 #it "releases a working bike" do
@@ -29,13 +34,5 @@ end
 #end
 
 it {is_expected.to respond_to(:dock).with(1).argument}
-
-it {is_expected.to respond_to(:bike)}
-
-it 'docks a bike' do
-  bike = Bike.new
-  subject.dock(bike)
-  expect(subject.bike).to eq bike
-end
 
 end
